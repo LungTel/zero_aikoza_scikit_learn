c7bb1f32a52facd526f20847d603cd785748a32e
github user LungTel <taiki50891@yahoo.co.jp>
2018-07-16 17:50:53 +0900

    20180716_新規ファイル追加_17時

--- stock_analysis.py
+++ stock_analysis.py
@@ -21,4 +21,48 @@ for i in range(1, count_s):
     modified_data.append(float(stock_data[i] - stock_data[i-1])/float(stock_data[i-1]) * 20)
 # print modified_data
 count_m = len(modified_data)
-print count_m
+# print count_m
+
+# 前日までの4連続の上昇率のデータ
+successive_data = []
+# 正解値　価格上昇:1, 価格低下:0
+answers = []
+for i in range(4, count_m):
+    successive_data.append([modified_data[i-4], modified_data[i-3], modified_data[i-2], modified_data[i-1]])
+    if modified_data[i] > 0:
+        answers.append(1)
+    else:
+        answers.append(0)
+# print successive_data
+# print answers
+
+# # データの数
+n = len(successive_data)
+# print n
+m = len(answers)
+# print m
+
+# 線形サポートベクターマシーン
+clf = svm.LinearSVC()
+# サポートベクターマシーンによる訓練(データの75%を訓練に使用)
+clf.fit(successive_data[:n*75/100], answers[:n*75/100])
+
+# テスト用データ
+# 正解
+expected = answers[-n*25/100:]
+# 予測
+predicted = clf.predict(successive_data[-n*25/100:])
+
+# 末尾の10個を比較
+print expected[-10:]
+print list(predicted[-10:])
+
+# 正解率の計算
+correct = 0.0
+wrong = 0.0
+for i in range(n*25/100):
+    if expected[i] == predicted[i]:
+        correct += 1
+    else:
+        wrong += 1
+print "正解率: " + str(correct / (correct + wrong) * 100) + "%"
